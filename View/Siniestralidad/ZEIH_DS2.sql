dcaa48ef80f349e7229e490810f88e583d515942
AnahiGJM18 <148823284+AnahiGJM18@users.noreply.github.com>
2025-08-13 11:00:41 -0600

    Modificacion

--- View/Siniestralidad/ZEIH_DS2.sql
+++ View/Siniestralidad/ZEIH_DS2.sql
@@ -1,40 +1,40 @@
 SELECT
-        "RECN_IAL",                                                                                     -----Número actual del registro de datos
-        "RECN_IP",                                                                                      -----Número actual del registro de datos
-        "IALID",                                                                                        -----Entrada de log de accidentes
-        "IATYPE",                                                                                       -----Clase de entrada log accidentes
-        "IAPLANT",                                                                                      -----Asignación de centro p.entrada en log
-        TO_DATE("EVDAT",'YYYYMMDD')             AS "Fecha del evento",                                  -----Fecha del evento
-        "EVTIME",                                                                                       -----Hora
-        "EVDESC",                                                                                       -----Descripción del evento
-        "ACLOCDESC",                                                                                    -----Descripción del lugar del accidente
-        TO_DATE("Z_EHS_FEC_NOTIF",'YYYYMMDD')   AS "Z_EHS_FEC_NOTIF" ,                                  -----Fecha notificación del accidente
-        TO_DATE("Z_EHS_FEC_INVES",'YYYYMMDD')   AS "Z_EHS_FEC_INVES",                                   -----Fecha de investigación
-        SUBSTRING("Z_EHS_AGNT_MAT",6)           AS "Z_EHS_AGNT_MAT",                                    -----Agente Material
-        SUBSTRING("Z_EHS_ECH_ANO",6)            AS "Hecho anormal que desencadenó",                     -----Hecho anormal que desencadenó
-        "IALPCAT",                                                                                      -----Categoría del accidente
-        "IALDEADFLG",                                                                                   -----Muerte
+        "RECN_IAL"                              AS "ZRECNIAL",                                                                                     -----Número actual del registro de datos
+        "RECN_IP"                               AS "ZRECNIP",                                                                                      -----Número actual del registro de datos
+        "IALID"                                 AS "0EHS_IAL",                                                                                     -----Entrada de log de accidentes
+        "IATYPE"                                AS "0EHS_IATP",                                                                                    -----Clase de entrada log accidentes
+        "IAPLANT"                               AS "ZIAPLANT",                                                                                     -----Asignación de centro p.entrada en log
+        TO_DATE("EVDAT",'YYYYMMDD')             AS "ZEVDAT",                                  -----Fecha del evento
+        "EVTIME"                                AS "ZEVTIME",                                                                                       -----Hora
+        "EVDESC"                                AS "0EHS_EVDE",                                                                                       -----Descripción del evento
+        "ACLOCDESC"                             AS "ZDESCLOC",                                                                                    -----Descripción del lugar del accidente
+        TO_DATE("Z_EHS_FEC_NOTIF",'YYYYMMDD')   AS "ZF_NOTIAL" ,                                  -----Fecha notificación del accidente
+        TO_DATE("Z_EHS_FEC_INVES",'YYYYMMDD')   AS "ZF_INVIAL",                                   -----Fecha de investigación
+        SUBSTRING("Z_EHS_AGNT_MAT",6)           AS "ZAGNMAT",                                    -----Agente Material
+        SUBSTRING("Z_EHS_ECH_ANO",6)            AS "ZECHANO",                     -----Hecho anormal que desencadenó
+        "IALPCAT"                               AS "0EHS_IACA",                                                                                      -----Categoría del accidente
+        "IALDEADFLG"                            AS "0EHS_IPDE",                                                                                   -----Muerte
         CASE 
-                "IPDEATHDATE",                                                                          -----Fecha de fallecimiento de la persona implicada
-        "IPABSESTDAYS",                                                                                 -----Duración estimada del absentismo (en días)
-        "IPABSDAYS",                                                                                    -----Cantidad de días laborables del absentismo
-        "IPABSCALDAYS",                                                                                 -----Días naturales
-        RIGHT("IPPERS",8)                       AS "Persona implicada",                                 -----Persona implicada
-        "GSBER",                                                                                        -----División
-        "DELFLG",                                                                                       -----Indicador de borrado
-        SUBSTRING("BODYPART",6)                 AS "BODYPART",                                          -----Parte del cuerpo
-        SUBSTRING("INJURY",6)                   AS "INJURY",                                            -----Traumatismo o enfermedad
+            WHEN "IPDEATHDATE" = '00000000'
+                THEN NULL 
+            ELSE TO_DATE("IPDEATHDATE",'YYYYMMDD')
+        END                                     AS "ZF_FALLEC",                                                                          -----Fecha de fallecimiento de la persona implicada
+        RIGHT("IPPERS",8)                       AS "0EMPLOYEE",                                 -----Persona implicada
+        SUBSTRING("BODYPART",6)                 AS "0EHS_DMTP",                                          -----Parte del cuerpo
+        SUBSTRING("INJURY",6)                   AS "0EHS_INJU",                                            -----Traumatismo o enfermedad
         CASE 
             WHEN "Z_EHS_FEC_BAJ" = '00000000'
                 THEN NULL
             ELSE TO_DATE("Z_EHS_FEC_BAJ",'YYYYMMDD')
-            END,                                                                                        -----Fecha baja
-        "Z_EHS_GRAV_ACC",                                                                               -----Gravedad del accidentado
-        "Z_EHS_TP_ASIST",                                                                               -----Tipo Asistencia
-        SUBSTRING("Z_EHS_TP_LUGAR",6)           AS "Z_EHS_TP_LUGAR",                                    -----Tipo de Lugar
-        SUBSTRING("Z_EHS_TP_TRABJ",6)           AS "Z_EHS_TP_TRABJ",                                    -----Tipo de Trabajo
-        SUBSTRING("Z_EHS_ACT_FIS",6)            AS "Z_EHS_ACT_FIS",                                     -----Actividad física específica
-        SUBSTRING("Z_EHS_FOR_CON",6)            AS "Z_EHS_FOR_CON",                                     -----Forma de contacto
+            END                                 AS "ZF_BAJA",                                                                                        -----Fecha baja
+        "Z_EHS_GRAV_ACC"                        AS "ZGRAVEDAD",                                                                               -----Gravedad del accidentado
+        "Z_EHS_TP_ASIST"                        AS "ZTPASISTIN",                                                                               -----Tipo Asistencia
+        SUBSTRING("Z_EHS_TP_LUGAR",6)           AS "ZTPLUGAR",                                    -----Tipo de Lugar
+        SUBSTRING("Z_EHS_TP_TRABJ",6)           AS "ZTPTRABJ",                                    -----Tipo de Trabajo
+        SUBSTRING("Z_EHS_ACT_FIS",6)            AS "ZACTFISC",                                     -----Actividad física específica
+        SUBSTRING("Z_EHS_FOR_CON",6)            AS "ZCONTACTO",                                     -----Forma de contacto
+        "ZOCUPAC"                               AS "ZOCUPAC",                                                                                      -----Ocupación PRL
+        SUM(1)                                  AS "ZCONTADO2"
         "Z_ULTIF_MODIFY",                                                                               -----Cronomarcador UTC en forma breve (AAAAMMDDhhmmss)
         "ZEDAD",                                                                                        -----ZEDAD
         "ZGENERO",                                                                                      -----ZGENERO
@@ -43,14 +43,18 @@ SELECT
         "Z_EHS_GRAD_LES",                                                                               -----Z_EHS_GRAD_LES
         "Z_EHS_GR_ACCTE",                                                                               -----Z_EHS_GR_ACCTE
         "ZZCEN_PRL",                                                                                    -----Código de centro de prevención
-        "ZOCUPAC",                                                                                      -----Ocupación PRL
         "ZREGION",                                                                                      -----Región/Grupo de Aeropuertos
         "ZGRUPOAAEE",                                                                                   -----Grupo Aropuertos
         "WERKS",                                                                                        -----División de personal
         "BTRTL",                                                                                        -----Subdivisión de personal
         YEAR(TO_DATE("EVDAT",'YYYYMMDD'))       AS "Año natural", 
         MONTH(TO_DATE("EVDAT",'YYYYMMDD'))      AS "Mes natural", 
-        QUARTER(TO_DATE("EVDAT",'YYYYMMDD'))
+        QUARTER(TO_DATE("EVDAT",'YYYYMMDD')),
+        "IPABSDAYS",                                                                                    -----Cantidad de días laborables del absentismo
+        "IPABSCALDAYS",                                                                                 -----Días naturales
+        "GSBER",                                                                                        -----División
+        "IPABSESTDAYS"                          AS "",                                                                                 -----Duración estimada del absentismo (en días)
+        "DELFLG",                                                                                       -----Indicador de borrado
         
 FROM
     "ECC_IL_HR_FuentSiniestralidad_RT"
