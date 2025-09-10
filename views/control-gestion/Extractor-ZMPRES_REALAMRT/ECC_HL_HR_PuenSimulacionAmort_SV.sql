fa301bc2f01410e1dc4d0e6ed9eee94a05df49ca
ANNSLZR <anettesalazar10@gmail.com>
2025-09-10 16:42:34 -0600

    Vista Ann

---
+++ views/control-gestion/Extractor-ZMPRES_REALAMRT/ECC_HL_HR_PuenSimulacionAmort_SV.sql
@@ -0,0 +1,13 @@
+SELECT 
+    "0CO_AREA",
+    "0CALMONTH2",
+    "0CALYEAR",
+    "0CALMONTH",
+    "DIVISION",
+    "ZCLASECTE",
+    "CECO",
+    "AREAVALOR",
+    "0CHRT_ACCTS",
+    "IMPTON2",
+    "0CURRENCY" 
+FROM "ECC_HL_HR_SimulacionAmortizacion_SV"
